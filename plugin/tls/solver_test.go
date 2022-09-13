bcbc181387d2f0d869a266087c398e43c0820c33
Marius Kimmina <mar.kimmina@gmail.com>
2022-09-13 16:37:07 +0200

    remove unnescessary whitespaces
    Signed-off-by: Marius Kimmina <mar.kimmina@gmail.com>

--- plugin/tls/solver_test.go
+++ plugin/tls/solver_test.go
@@ -60,7 +60,6 @@ func TestSolveChallenge(t *testing.T) {
 	<-readyChan
 	for _, tc := range testcases {
 		t.Run(tc.name, func(t *testing.T) {
-
 			m := new(dns.Msg)
 			m.SetQuestion(tc.question, dns.TypeTXT)
 			resp, err := dns.Exchange(m, "127.0.0.1:2053")
@@ -76,7 +75,6 @@ func TestSolveChallenge(t *testing.T) {
 					t.Fatalf("Expected to receive no RR in the answer section, got at least one")
 				}
 			}
-
 		})
 	}
 }
